1b82058e8bf7a14df0db998bc873c64534254848
DShalygin <DShalygin@it.ru>
2016-10-06 11:01:54 +0700

    regEx Replacment ${}

--- test/TIsForTest.java
+++ test/TIsForTest.java
@@ -1,3 +1,4 @@
+import javafx.print.Collation;
 import org.junit.Test;
 
 import org.w3c.dom.Document;
@@ -9,6 +10,12 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.*;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -16,27 +23,20 @@ import java.io.InputStream;
  */
 public class TIsForTest {
 
+//    private static Pattern PATTERN = Pattern.compile("(?:\\$\\{([^${}]+)\\})+?");
+    private static Pattern PATTERN = Pattern.compile("\\$\\{(.*?)\\}");
+
     @Test
     public void main() {
         try {
-            InputStream inputStream = TIsForTest.class.getResourceAsStream("Persons.XML");
+            Matcher matcher = PATTERN.matcher("sdf_${ATTR1}_dsfsd${ATTR2}${ATTR3}");
 
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder;
-            Document doc;
-            try {
-                builder = factory.newDocumentBuilder();
-                doc = builder.parse(inputStream);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            StringBuffer sb = new StringBuffer();
+            while (matcher.find()) {
+                matcher = matcher.appendReplacement(sb, matcher.group(1));
             }
 
-            XPath xpath = XPathFactory.newInstance().newXPath();
-
-            String expression = "//RECORD/FIELD[@NAME='NREC' and text() ='00150000000014B9h' and @NAME='PERSNUMBER' or text() ='30100009']";
-
-            System.out.println(xpath.compile("../FIELD[@NAME='FULLNAME']").evaluate(xpath.compile(expression).evaluate(doc, XPathConstants.NODE), XPathConstants.STRING));
-
+            System.out.println(sb);
         } catch (Throwable e) {
             System.out.println(e);
         }
