f8eb8e36da89e96dc2c2051b5305ef18e521fa74
DShalygin <DShalygin@it.ru>
2018-09-20 11:08:47 +0700

    4

---
+++ src/main/java/ru/shal1928/Test.java
@@ -0,0 +1,21 @@
+package ru.shal1928;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+
+/**
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, ParseException {
+
+        try {
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
